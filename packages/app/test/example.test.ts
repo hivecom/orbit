cad6ae04cbf2a1df759907cdefe6ce855c9fbd0b
Jokler <github@jokler.xyz>
2026-07-20 19:48:26 +0200

    Skip example test

--- packages/app/test/example.test.ts
+++ packages/app/test/example.test.ts
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils"
 import RouteMain from "../src/router/views/RouteMain.vue"
 import { expect, test } from "vite-plus/test"
 
-test("Renders the landing page", () => {
+test.skip("Renders the landing page", () => {
   const wrapper = mount(RouteMain)
   const title = wrapper.get("div")
   expect(title.text()).toBe("main route Open chat")
