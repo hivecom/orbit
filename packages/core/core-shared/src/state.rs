fb4d59dfae4bb1dca5aaaea27155c3210ad1ba22
Jokler <github@jokler.xyz>
2026-08-11 19:29:03 +0200

    Adjust MessageReference doc comment

--- packages/core/core-shared/src/state.rs
+++ packages/core/core-shared/src/state.rs
@@ -550,9 +550,9 @@ impl Eq for MessageMetadata {}
 #[cfg_attr(feature = "web", derive(Tsify))]
 #[cfg_attr(feature = "web", wasm_bindgen(getter_with_clone, inspectable))]
 pub struct MessageReference {
-    /// Empty if message wasn't found or if reply wasn't to a text message
+    /// Unset if message wasn't found or if reply wasn't to a text message
     pub text: Option<String>,
-    /// Empty if message wasn't found
+    /// Unset if message wasn't found
     pub username: Option<String>,
 }
 
