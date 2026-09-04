be90d88b9f046864840e96583e91edaece683f0b
dolanske <dolanovsky@gmail.com>
2026-09-04 10:11:30 +0300

    Temporarily use web platform for desktop

--- packages/platform/src/desktop.ts
+++ packages/platform/src/desktop.ts
@@ -1,7 +1,12 @@
 import { createMockPlatform } from "./mock"
 import type { Platform } from "./types"
 
-// TODO: implement
+// TODO: This needs to be implemented immediately when Tauri work begins. For
+// now we fallback to the web implementation and changing the target as per
+// @zealprince's suggestion
 export function createDesktopPlatform(): Platform {
-  return createMockPlatform("desktop")
+  return {
+    ...createMockPlatform("web"),
+    target: "desktop",
+  }
 }
